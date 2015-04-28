4b94111ea7f3c713f91421344940b16d894ee521
WeWeDe <gijsweterings@gmail.com>
2015-04-28 12:12:36 +0200

    Moved the loading of a song to a function.
    We can now press a button to load song #2.
     This calls a function with 1 argument: the soundcloud track id.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,8 +34,28 @@ angular
         redirectTo: '/'
       });
   });
+
+
+
   angular.module('moodCatAudio', ['ngAudio'])
-    .controller('AudioCtrl', function($scope, ngAudio) {
-      $scope.sound = ngAudio.load("https://api.soundcloud.com/tracks/13158665/stream?client_id=68d16250d1ef387ae1000e9102a23ddb");
-      $scope.sound.title = "Fall Out Boy - Centuries";
-    });
+    .value("soundCloudKey", "68d16250d1ef387ae1000e9102a23ddb")
+    .value("track", "202330997")
+    .controller('AudioCtrl', ["$scope", "ngAudio", "soundCloudKey", "track", "$http", function($scope, ngAudio, soundCloudKey, track, $http) {
+      $scope.loadSong = function(trackID) {
+        if (typeof($scope.sound) !== 'undefined') {
+          $scope.sound.stop();
+          $scope.sound.unbind();
+        }
+        $http.get('https://api.soundcloud.com/tracks/'+trackID+'?client_id='+soundCloudKey).success(function(data, status, headers, config) {
+          console.log(JSON.stringify(data.title));
+          console.log(JSON.stringify('Song will play for  ' + data.duration + 'ms'));
+          var sound = ngAudio.load("https://api.soundcloud.com/tracks/"+trackID+"/stream?client_id="+soundCloudKey);
+          sound.title = data.title;
+          $scope.sound = sound;
+          $scope.sound.play();
+        });
+      }
+
+      $scope.loadSong(track);
+
+    }]);
