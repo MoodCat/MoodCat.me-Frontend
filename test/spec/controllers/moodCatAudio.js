23c8d4909345759da5a23fab48a262ccdffbad3e
WeWeDe <gijsweterings@gmail.com>
2015-04-28 20:22:24 +0200

    test not working. Need JW to look at this with me tomorrow

---
+++ test/spec/controllers/moodCatAudio.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Controller: AudioCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('moodCatAudio'));
+
+  var AudioCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    AudioCtrl = $controller('AudioCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('After loading a song, the song object should be defined', function () {
+    scope.loadSong('202330997');
+  //  console.log(scope);
+  //  expect(scope.sound).toBeDefined();
+  });
+});
