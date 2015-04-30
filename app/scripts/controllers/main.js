d5bb8982aa812a3598ccf1d8f57dc91f494a3c98
Jan-Willem Gmelig Meyling <j.gmeligmeyling@student.tudelft.nl>
2015-05-01 00:27:30 +0200

    Initial commit of room UI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,55 @@
  * Controller of the moodCatApp
  */
 angular.module('moodCatApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout, soundCloudService) {
+    $scope.moods = ['Angry', 'Nervous', 'Exiting', 'Happy', 'Pleasing', 'Relaxing',
+      'Peaceful', 'Calm', 'Sleepy', 'Bored', 'Sad'];
+
+    $scope.rooms = [];
+
+    soundCloudService.fetchMetadata("202846566").success(function(data) {
+      $scope.rooms = ['Copperhead Toads', 'Macho Junkers', 'Long Cat Force', 'Damaged Bush Supernovas',
+        'Spinning Rag Razors', 'Sweeping Blank Cicadas', 'Silver Ass Wankers', 'The Swamp Epidemic',
+        'The Dope Samaritans', 'Silver Milk Dribblers', 'Great Surf Flux', 'Los Caviar Roosters',
+        'Seaview Purple Patsies'].map(function (name) {
+          return {
+            name: name,
+            song: data
+          }
+        });
+      $scope.activeRoom = $scope.rooms[0];
+    })
+
+    $scope.messages = ['Hoihoi!'].map(function(message) {
+      return {
+        author: "Jan-Willem",
+        message: message,
+        time: (new Date()).valueOf()
+      }
+    });
+
+    $scope.chatMessage = {
+      message: "",
+      author: "Eva"
+    };
+
+    $scope.addMessage = function() {
+      $scope.messages.push({
+        message: $scope.chatMessage.message,
+        author: $scope.chatMessage.author,
+        time: (new Date()).valueOf()
+      });
+      $scope.chatMessage.message = "";
+
+      $timeout(function() {
+        var list = angular.element("#chat-messages-list")[0];
+        list.scrollTop = list.scrollHeight;
+      })
+    }
+
+    $scope.selectRoom = function selectRoom(room) {
+      $scope.activeRoom = room;
+      $scope.audioCtrl.loadSong(room.song.id);
+    }
+
   });
--- app/scripts/controllers/moodCatAudio.js
+++ app/scripts/controllers/moodCatAudio.js
@@ -8,43 +8,96 @@
  * Controller of the audio of moodcat
  */
  angular.module('moodCatAudio')
-   .value('soundCloudKey', '68d16250d1ef387ae1000e9102a23ddb')
+   .value('soundCloudKey', 'cef809be114bdf9f856c735139f2aeba')
    .value('track', '202330997')
-   .controller('AudioCtrl', ['$scope', 'ngAudio', 'soundCloudKey', 'track', '$http', function($scope, ngAudio, soundCloudKey, track, $http) {
-      $scope.unloadSong = function() {
-         //console.log(typeof($scope.sound));
-        if (typeof $scope.sound !== 'undefined' && $scope.sound !== null) {
-          $scope.sound.stop();
-          $scope.sound.unbind();
-        }
-     };
+   .service('soundCloudService', ['soundCloudKey', '$http', function(soundCloudKey, $http) {
 
-      $scope.metaData = function(trackID) {
-         var promise = $http.get('https://api.soundcloud.com/tracks/'+trackID+'?client_id='+soundCloudKey);
+     /**
+      * Fetch the metadata for a song
+      * @param trackID
+      * @returns {*} Promise
+      */
+     this.fetchMetadata = function fetchMetadata(trackID) {
+       return $http.get('https://api.soundcloud.com/tracks/'+trackID+'?client_id='+soundCloudKey);
+     }
 
-         // Works Async, so the title will be set when it'll be set.
-         // No waiting for slow JSON responses
-         return promise.then(
-            function(payload) {
-               $scope.sound.title = payload.data.title;
-               console.log(payload.data.title);
-            }
-         );
-      };
+   }])
+   .controller('AudioCtrl', ['$scope', 'ngAudio', 'soundCloudKey', 'track', '$http', '$log', 'soundCloudService', function($scope, ngAudio, soundCloudKey, track, $http, $log, soundCloudService) {
 
-      $scope.loadSong = function(trackID) {
-         var sound = null;
+     /**
+      * Pad a string with zeroes
+      * @param str string to pad
+      * @param max wanted string length
+      * @returns {String} padded string
+      */
+     function pad (str, max) {
+       str = str.toString();
+       return str.length < max ? pad("0" + str, max) : str;
+     }
 
-         // Unloads old song, saving some clientside RAM
-         // and preventing old songs from playing
-         $scope.unloadSong();
-         $scope.metaData(trackID);
-         // Load NgAudioObject with specified path
-         sound = ngAudio.load('https://api.soundcloud.com/tracks/'+trackID+'/stream?client_id='+soundCloudKey);
+     /**
+      * Load a song
+      * @param trackID
+      * @returns {*} Promise
+      */
+     this.loadSong = function loadSong(trackID) {
+       if(angular.isObject($scope.sound)) {
+         $scope.sound.stop();
+         $scope.sound.unbind();
+         $scope.sound = null;
+       }
 
-         $scope.sound = sound;
+       return soundCloudService.fetchMetadata(trackID).success(function(data) {
+         $scope.song = data;
+         $log.info("Playing song %s", data.title);
+         $scope.sound = ngAudio.load('https://api.soundcloud.com/tracks/'+trackID+'/stream?client_id='+soundCloudKey);
+         //$scope.sound.play();
+       });
+     }
 
-      };
+     Object.defineProperty($scope, 'timestamp', {
+       get: function() {
+         var val = 0.0, seconds, minutes, millis, hours;
+         if(angular.isObject($scope.sound)) {
+           val = $scope.sound.currentTime;
+         }
+         seconds = Math.floor(val);
+         minutes = Math.floor(seconds / 60);
+         hours = Math.floor(minutes / 60);
+         minutes -= hours * 60;
+         //millis = Math.round((val - seconds) * 1000);
+         seconds -= minutes * 60;
 
-     $scope.loadSong(track);
-   }]);
+         return pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2);// + "." + pad(millis, 3);
+       }
+     });
+
+     this.loadSong(track);
+   }])
+   .controller("SoundCloudController", ['soundCloudKey', '$scope', function(soundCloudKey, $scope) {
+     SC.initialize({
+       client_id: soundCloudKey,
+       redirect_uri: window.location.origin // Only works from localhost:8080 currently
+     });
+
+     this.loggedIn = false;
+
+     this.loginUsingSoundcloud = function loginUsingSoundcloud() {
+       SC.connect((function() {
+         SC.get('/me', (function(me) {
+           this.loggedIn = true;
+           this.me = me;
+         }).bind(this));
+       }).bind(this));
+     }
+   }])
+   .directive('btnMood', function() {
+     return {
+       restrict: 'E',
+       transclude: true,
+       scope: {
+         ngModel : '='
+       },
+       template: '<label noselect><input type="checkbox" ng-model="ngModel"/><span ng-transclude class="mood-label"></span></label>'
+     }
+   });
