ef6346239802195ae77df04050f58f64983d7f59
Jaap <jaapheijligers@gmail.com>
2015-06-04 12:11:38 +0200

    Added deployment bash script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Pulling latest version..."
+git pull > /dev/null || { echo "Git pull failed, exiting"; exit 1; }
+echo "Executing grunt build..."
+grunt build > /dev/null || { echo "Grunt build failed, exiting"; exit 1; }
+echo "Copying needed files to dist/..."
+cp -r app/mocks dist/
+cp -r bower_components/ui-router-extras dist/bower_components/
+cp -r app/images/arousal/* dist/images/arousal/
+cp -r app/images/valence/* dist/images/valence/
+echo "Removing /usr/share/nginx/html/dist"
+ssh moodcat@moodcat 'rm -r /usr/share/nginx/html/dist' || { echo "Connecting to server failed, exiting"; exit 1; }
+echo "Deploying new dist/"
+scp -r dist moodcat@moodcat:/usr/share/nginx/html/dist/ > /dev/null
+echo "Done."
+exit 0
